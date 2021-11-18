ea6e033f14e33758c33af930d80395e2684d4326
Lazizbek <65546555+lazizbekravshanov@users.noreply.github.com>
2021-11-18 14:40:10 -0500

    Create code.py

---
+++ A+B(0001)/code.py
@@ -0,0 +1,11 @@
+# input: 2 3
+# output: 5
+
+a,b=map(int,input().split())
+print(a+b)
+
+# How it works?
+# Initially, I thought that it was quite easy to solve the problem, but after many trials I got errors ranging from presentation error to runtime error.
+# What makes difference is the space when checking the code. The code should run when you test it with space in between numbers. 
+# You can read about the use of map() here https://stackoverflow.com/questions/1247486/python-list-comprehension-vs-map
+# Also, look at this https://www.geeksforgeeks.org/input-multiple-values-user-one-line-python/
